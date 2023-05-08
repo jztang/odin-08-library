7b66e0e8bd3c4a01168bb2be46a2c35e05a8de78
jztang <12689662+jztang@users.noreply.github.com>
2023-05-08 15:24:04 -0400

    Style library

--- main.js
+++ main.js
@@ -12,33 +12,43 @@ function addBookToLibrary(author, title, pages, read) {
   myLibrary.sort((a, b) => a.author.localeCompare(b.author) || a.title.localeCompare(b.title));
 }
 
+function generateCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("div");
+  title.classList.add("title");
+  title.textContent = book.title;
+  card.appendChild(title);
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  author.textContent = book.author;
+  card.appendChild(author);
+
+  const bottomRow = document.createElement("div");
+  bottomRow.classList.add("bottom-row");
+  const pages = document.createElement("div");
+  pages.classList.add("pages");
+  pages.textContent = `${book.pages} pages`;
+  bottomRow.appendChild(pages);
+  const read = document.createElement("div");
+  read.classList.add("read");
+  if (book.read) {
+    read.textContent = "Read";
+  } else {
+    read.textContent = "Unread";
+  }
+  bottomRow.appendChild(read);
+  card.appendChild(bottomRow);
+
+  return card;
+}
+
 function displayLibrary() {
   const library = document.querySelector(".library");
   for (const book of myLibrary) {
-    const bookCard = document.createElement("div");
-
-    const title = document.createElement("div");
-    title.textContent = book.title;
-    bookCard.appendChild(title);
-
-    const author = document.createElement("div");
-    author.textContent = `by ${book.author}`;
-    bookCard.appendChild(author);
-
-    const bottomRow = document.createElement("div");
-    const pages = document.createElement("div");
-    pages.textContent = `${book.pages} pages`;
-    bottomRow.appendChild(pages);
-    const read = document.createElement("div");
-    if (book.read) {
-      read.textContent = "Read";
-    } else {
-      read.textContent = "Unread";
-    }
-    bottomRow.appendChild(read);
-    bookCard.appendChild(bottomRow);
-
-    library.appendChild(bookCard);
+    library.appendChild(generateCard(book));
   }
 }
 
